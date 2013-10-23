7cdfd8d95c4f2f3281e19feb81951b2aa9ea6e15
AlejandroGomez92 <oldreliable92@yahoo.com>
2013-10-22 19:52:48 -0700

    EvenOrOddLab
    Even or Odd class and methods

---
+++ Week9Lab1EvenOdd/DriverEvenOdd.java
@@ -0,0 +1,10 @@
+
+public class DriverEvenOdd {
+	public static void main(String[] args ){
+		
+		EvenOdd myEvenOdd = new EvenOdd();
+		
+		myEvenOdd.displayMessageOdd();
+		myEvenOdd.displayMessageEven();
+	}
+}
---
+++ Week9Lab1EvenOdd/EvenOdd.java
@@ -0,0 +1,14 @@
+
+public class EvenOdd {
+	
+	public void displayMessageOdd()
+	{
+		System.out.println("Odd!");
+	}
+	
+	
+	public void displayMessageEven(){
+		
+		System.out.println("Even!");
+	}
+}
